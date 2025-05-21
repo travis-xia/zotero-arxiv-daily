ef77b07b71efd0aff5a66dcf7c2debe7ad930964
Sheng Xia <78533308+travis-xia@users.noreply.github.com>
2025-05-21 13:40:42 +0800

    Update paper.py

--- paper.py
+++ paper.py
@@ -171,7 +171,7 @@ class ArxivPaper:
         # use gpt-4o tokenizer for estimation
         enc = tiktoken.encoding_for_model("gpt-4o")
         prompt_tokens = enc.encode(prompt)
-        prompt_tokens = prompt_tokens[:4000]  # truncate to 4000 tokens
+        prompt_tokens = prompt_tokens[:3200]  # truncate to 4000 tokens
         prompt = enc.decode(prompt_tokens)
         
         tldr = llm.generate(
